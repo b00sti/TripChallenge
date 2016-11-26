7db1a07b9bcd790ba042143b7363e29de390d7d9
b00sti <pawlik.dominik@gmail.com>
2016-11-26 11:02:13 +0100

    clean simple

--- app/src/main/java/com/example/b00sti/tripchallenge/main/InnerViewActivity.java
+++ app/src/main/java/com/example/b00sti/tripchallenge/main/InnerViewActivity.java
@@ -1,13 +1,9 @@
 package com.example.b00sti.tripchallenge.main;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.b00sti.tripchallenge.R;
-import com.example.skeleton.android_utils.util.ViewUtils;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
@@ -32,15 +28,6 @@ public class InnerViewActivity extends com.example.skeleton.ui.activity_utils.Ba
     @Override
     public Fragment setFragment(@FragmentBuilder.FragBuild int fragmentId) {
         return fragmentBuilder.newFragment(fragmentId);
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        Log.d(TAG, "onCreate: kurwa");
-        ViewUtils.showToast(this, "KURWA !");
-        super.onCreate(savedInstanceState, persistentState);
-        ViewUtils.showToast(this, "KURWA !");
-        Log.d(TAG, "onCreate: kurwa");
     }
 
     @Override
