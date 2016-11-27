65d9bac3b36cbb24f8ff8183cbf7f062ccd35db2
b00sti <pawlik.dominik@gmail.com>
2016-11-27 02:18:58 +0100

    add dialog creator class

--- app/src/main/java/com/example/b00sti/tripchallenge/ui_login/LogInPresenter.java
+++ app/src/main/java/com/example/b00sti/tripchallenge/ui_login/LogInPresenter.java
@@ -1,20 +1,17 @@
 package com.example.b00sti.tripchallenge.ui_login;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.b00sti.tripchallenge.R;
 import com.example.b00sti.tripchallenge.main.FragmentBuilder;
 import com.example.b00sti.tripchallenge.utils.ActivityUtils;
+import com.example.b00sti.tripchallenge.utils.helpers.DialogCreator;
 import com.example.skeleton.android_utils.eventbus.SwitchDrawerFragmentEvent;
 import com.example.skeleton.android_utils.firebase.FirebaseManager;
 import com.example.skeleton.android_utils.navigation.drawer.DrawerUtils;
-import com.example.skeleton.android_utils.util.ViewUtils;
 import com.example.skeleton.ui.mvp_base.MvpPresenter;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,8 +21,6 @@ import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.res.ColorRes;
 import org.greenrobot.eventbus.EventBus;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * Created by b00sti on 20.11.2016
@@ -55,71 +50,41 @@ public class LogInPresenter extends MvpPresenter<LogInContract.View> implements 
 
     @Override
     public void afterForgotPassword() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
 
-        //builder.setTitle("Reset Password");
-        View view = View.inflate(ctx, R.layout.dialog_forgot_password, null);
-        View view2 = View.inflate(ctx, R.layout.dialog_title, null);
-        EditText input = (EditText) view.findViewById(R.id.dialog_editText);
-        builder.setView(view);
-        builder.setCustomTitle(view2);
-
-// Set up the buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String email = input.getText().toString();
-                if (email.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-                    builder.setMessage(R.string.login_error_message)
-                            .setTitle(R.string.login_error_title)
-                            .setPositiveButton(android.R.string.ok, null);
-                    AlertDialog dialog2 = builder.create();
-                    dialog2.show();
-                } else {
-                    firebaseAuth.sendPasswordResetEmail(email)
-                            .addOnCompleteListener(ctx, task -> {
-                                if (task.isSuccessful()) {
-                                    ViewUtils.hideKeyboard(ctx);
-                                    ViewUtils.showToast(ctx, "Check your email !");
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-                                    builder.setMessage("Check your email for the link to reset your password")
-                                            .setTitle("Password Request Send")
-                                            .setPositiveButton(android.R.string.ok, null);
-                                    AlertDialog dialog2 = builder.create();
-                                    dialog2.show();
-                                } else {
-                                    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-                                    Exception exception = task.getException();
-                                    String message = exception != null ? exception.getMessage() : "";
-                                    builder.setMessage(message)
-                                            .setTitle(R.string.login_error_title)
-                                            .setPositiveButton(android.R.string.ok, null);
-                                    AlertDialog dialog3 = builder.create();
-                                    dialog3.show();
-                                }
-                            });
-                }
+        DialogCreator forgotPasswordDialog = new DialogCreator(ctx);
+        forgotPasswordDialog.setTitle("Forgot");
+        forgotPasswordDialog.setMessage("Enter Email");
+        forgotPasswordDialog.setMessageViewId(R.layout.dialog_forgot_password);
+        EditText input = (EditText) forgotPasswordDialog.getMessageView().findViewById(R.id.dialog_editText);
+        TextView message = (TextView) forgotPasswordDialog.getMessageView().findViewById(R.id.dialog_textView);
+        message.setText(forgotPasswordDialog.getMessage());
+        forgotPasswordDialog.setPositiveButtonListener((dialogInterface, i) -> {
+            String email = input.getText().toString();
+            if (email.isEmpty()) {
+                new DialogCreator(ctx).show(R.string.login_error_title, R.string.login_error_message, (dialogInterface1, i1) -> {
+                }, null);
+            } else {
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(ctx, task -> {
+                            if (task.isSuccessful()) {
+                                new DialogCreator(ctx).show("Password Request Send", "Check your email for the link to reset your password", (dialogInterface1, i1) -> {
+                                }, null);
+                            } else {
+                                Exception exception = task.getException();
+                                String exceptionMessage = exception != null ? exception.getMessage() : "";
+                                new DialogCreator(ctx).show(R.string.login_error_title, exceptionMessage, (dialogInterface1, i1) -> {
+                                }, null);
+                            }
+                        });
             }
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        AlertDialog dialog = builder.create();
-        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
-        dialog.show();
-        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(lightTextColor);
-        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(lightTextColor);
+        forgotPasswordDialog.setNegativeButtonListener((dialogInterface, i) -> dialogInterface.cancel());
+        forgotPasswordDialog.show();
 
     }
 
     @Override
     public void afterNoAccount() {
-        Log.d(TAG, "afterNoAccount: ");
         ActivityUtils.startInnerViewActivity(ctx, FragmentBuilder.LOG_IN);
     }
 
@@ -130,12 +95,8 @@ public class LogInPresenter extends MvpPresenter<LogInContract.View> implements 
         password = password.trim();
 
         if (email.isEmpty() || password.isEmpty()) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-            builder.setMessage(R.string.login_error_message)
-                    .setTitle(R.string.login_error_title)
-                    .setPositiveButton(android.R.string.ok, null);
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            new DialogCreator(ctx).show(R.string.login_error_title, R.string.login_error_message, (dialogInterface1, i1) -> {
+            }, null);
         } else {
             firebaseAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(ctx, task -> {
@@ -143,14 +104,10 @@ public class LogInPresenter extends MvpPresenter<LogInContract.View> implements 
                             Fragment targetFragment = fragmentBuilder.newFragment(FragmentBuilder.DASHBOARD);
                             EventBus.getDefault().post(new SwitchDrawerFragmentEvent(targetFragment, DrawerUtils.TAB_00));
                         } else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
                             Exception exception = task.getException();
-                            String message = exception != null ? exception.getMessage() : "";
-                            builder.setMessage(message)
-                                    .setTitle(R.string.login_error_title)
-                                    .setPositiveButton(android.R.string.ok, null);
-                            AlertDialog dialog = builder.create();
-                            dialog.show();
+                            String exceptionMessage = exception != null ? exception.getMessage() : "";
+                            new DialogCreator(ctx).show(R.string.login_error_title, exceptionMessage, (dialogInterface1, i1) -> {
+                            }, null);
                         }
                     });
         }
