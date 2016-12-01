7b588acc88ecf18d89458003bd72e24a6ebf6ca6
b00sti <pawlik.dominik@gmail.com>
2016-12-01 09:08:37 +0100

    clean before pull

--- skeleton/src/main/java/com/example/skeleton/ui/activity_utils/BaseInnerViewActivity.java
+++ skeleton/src/main/java/com/example/skeleton/ui/activity_utils/BaseInnerViewActivity.java
@@ -2,11 +2,8 @@ package com.example.skeleton.ui.activity_utils;
 
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.skeleton.R;
@@ -14,21 +11,21 @@ import com.example.skeleton.android_utils.navigation.fragment_switching.Fragment
 import com.example.skeleton.android_utils.navigation.fragment_switching.FragmentSwitcherParams;
 import com.example.skeleton.android_utils.util.ViewUtils;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 /**
  * Created by Dominik (b00sti) Pawlik on 2016-11-15
  */
 
-@EActivity(resName = "activity_inner_view")
+@EActivity
 public abstract class BaseInnerViewActivity extends AppCompatActivity {
     private static final String TAG = "BaseInnerViewActivity";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        Log.d(TAG, "onCreate: ");
+    @AfterViews
+    public void setFragment() {
         setInitialFragment(getIntent());
+        setTitle(getIntent());
     }
 
     @Override
@@ -39,7 +36,6 @@ public abstract class BaseInnerViewActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case android.R.id.home:
                 onBackPressed();
@@ -49,8 +45,14 @@ public abstract class BaseInnerViewActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setInitialFragment(Intent intent) {
-        Log.d(TAG, "setInitialFragment: ");
+    private void setTitle(Intent intent) {
+        if (intent.hasExtra("name")) {
+            String s = intent.getStringExtra("name");
+            setTitle(s);
+        }
+    }
+
+    private void setInitialFragment(Intent intent) {
         int fragmentId;
         if (intent.hasExtra(getString(R.string.bundle_fragment))) {
             fragmentId = intent.getIntExtra(getString(R.string.bundle_fragment), -1);
@@ -61,9 +63,7 @@ public abstract class BaseInnerViewActivity extends AppCompatActivity {
     public abstract Fragment setFragment(int fragmentId);
 
     private void switchToFragment(int fragmentId) {
-        Log.d(TAG, "switchToFragment: ");
         Fragment fragment = setFragment(fragmentId);
-        Log.d(TAG, "switchToFragment: " + fragment.getTag());
         FragmentSwitcher.switchFragment(new FragmentSwitcherParams(getSupportFragmentManager(), fragment, R.id.activity_inner_view_placeholder), this);
     }
 }
