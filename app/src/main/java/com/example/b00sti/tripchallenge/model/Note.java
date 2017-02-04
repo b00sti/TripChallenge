110b62f35d4dae2cbbc9ec3c863fe72ace5350e0
b00sti <pawlik.dominik@gmail.com>
2017-02-04 22:23:42 +0100

    added lombok

--- app/src/main/java/com/example/b00sti/tripchallenge/model/Note.java
+++ app/src/main/java/com/example/b00sti/tripchallenge/model/Note.java
@@ -1,11 +1,15 @@
 package com.example.b00sti.tripchallenge.model;
 
 import io.realm.RealmObject;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by Dominik (b00sti) Pawlik on 2017-01-12
  */
 
+@Getter
+@Setter
 public class Note extends RealmObject {
     private String note;
 }
